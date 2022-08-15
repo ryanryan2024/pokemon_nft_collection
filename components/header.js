3eb5e970b12976e307147b1bb2b2186da0a37337
Ryan <76850473+ryanryan2024@users.noreply.github.com>
2022-08-15 05:23:56 -0700

    Add files via upload

---
+++ components/header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="flex justify-center py-10 px-2">
+            <h1 className="font-sans font-extralight">
+                Explore the Pokémon NFT collection in many unique colors.
+            </h1>
+        </div>
+    )
+}
+
+export default Header
