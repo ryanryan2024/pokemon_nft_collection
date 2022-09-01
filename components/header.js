ebd2b0d29a79020c5ba555536b99696b15dc72a4
Ryan <76850473+ryanryan2024@users.noreply.github.com>
2022-08-31 18:19:32 -0700

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,12 +1,12 @@
 const Header = () => {
     return (
-        <div className="flex justify-center py-10 px-2">
-            <div>
+        <div className="py-10 px-2">
+            <div className="flex justify-center">
                 <h1 className="font-sans font-extralight">
                     Explore the Pokémon NFT collection in many unique colors.
                 </h1>
             </div>
-            <div>
+            <div className="flex justify-center">
                 <h1 className="font-sans font-extralight">
                     Note: If you would like to exchange in a different currency, <a href="https://mvrnft.netlify.app/contact.html">contact us.</a>
                 </h1>
